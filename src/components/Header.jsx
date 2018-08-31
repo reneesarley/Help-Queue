a0a0fb722444ecc9be8ed2f4ed8da98a753c75ca
Renee Sarley <reneesarley@gmail.com>
2018-08-31 10:19:59 -0700

    add image and image loader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/images/confusedGuy.jpg';
+
 
 function Header() {
   var headerStyles = {
     textAlign: 'center'
-  }
+  };
   return(
     <div>
+      <img src={logo} width='30%' height='30%'/>
       <h1 style={headerStyles}>Help Queue!</h1>
       <p> <Link to='/'>Home</Link> | <Link to='/newticket'>Create New Ticket</Link></p>
     </div>
