2bec2f813027e4b86646c1d25c07313a57333d23
Renee Sarley <reneesarley@gmail.com>
2018-09-01 15:13:09 -0700

    update new ticket form

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 function NewTicketForm (){
+  let _names = null;
+  let _location = null;
+  let _issue = null;
+
+  function handleNewTicketFormSubmission(event){
+    event.preventDefault();
+    console.log(_names.value + _location.value  + _issue.value)
+    _names.value = '';
+    _location.value= '';
+    _issue.value= '';
+  }
   return (
     <div>
-      <form>
-        <input type='text' id='names' placeholder='Pair Names'/>
-        <input type='text' id='location' placeholder='Location'/>
-        <textarea id='area' placeholder='Describe your issue'/>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <input type='text' id='names' placeholder='Pair Names' ref={(input) => {_names = input;}}/>
+        <input type='text' id='location' placeholder='Location' ref={(input) => {_location = input;}}/>
+        <textarea id='area' placeholder='Describe your issue' ref={(textarea) => {_issue = textarea;}}/>
         <button type='submit'>Help!</button>
       </form>
     </div>
